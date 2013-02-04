704aa725db621719880ff21be70670aded428ef6
Droogans <droogans@gmail.com>
2013-02-03 21:40:09 -0500

    return ' ' joined string, not an array

--- rosalind/01 - DNA/dna.clj
+++ rosalind/01 - DNA/dna.clj
@@ -1,3 +1,4 @@
 (defn dna [sequence]
   "Returns the count of each member in a DNA sequence, (A C G T)"
-  (vals (sort-by key (frequencies (seq sequence)))))
+  (clojure.string/join " "
+    (vals (sort-by key (frequencies (seq sequence))))))
