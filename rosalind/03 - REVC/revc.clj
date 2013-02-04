27677b645001abd1d18e1528abb12f9536b573e8
Droogans <droogans@gmail.com>
2013-02-03 21:40:32 -0500

    style changes

--- rosalind/03 - REVC/revc.clj
+++ rosalind/03 - REVC/revc.clj
@@ -1,7 +1,9 @@
 (defn revc [sequence]
-  (clojure.string/join "" (reverse (clojure.string/replace 
-                                     sequence #"A|C|G|T"
-                                     {"A" "T"
-                                      "C" "G"
-                                      "G" "C"
-                                      "T" "A"}))))
+  "Reverse a DNA sequence, replacing ACGT with complement TGCA"
+  (clojure.string/join ""
+    (reverse (clojure.string/replace
+      sequence #"A|C|G|T"
+        {"A" "T"
+         "C" "G"
+         "G" "C"
+         "T" "A"}))))
